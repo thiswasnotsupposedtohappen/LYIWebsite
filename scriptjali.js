21b6767a7c801bdb48013354445c4a9e2330641f
thiswasnotsupposedtohappen <abhimanyumidnight@hotmail.com>
2024-11-14 14:19:42 +0530

    0.0.2

--- scriptjali.js
+++ scriptjali.js
@@ -21,13 +21,13 @@ for(var i=1;i<=30;i++)
 }
 
 for(var i=1;i<=20;i++)  
-    {
-        shop_menu_innerhtml += `
-        <div class="shop-menu-items">
-          <a href="shop-jali.html"><img class="shop-menu-items-img" src="images/Decorative Jali/Mandir side/`+i+`.jpg"></a><br>
-          <h2>MandirSide-`+i+`</h2>
-        </div>
-        `;
-    }
+{
+    shop_menu_innerhtml += `
+    <div class="shop-menu-items">
+      <a href="shop-jali.html"><img class="shop-menu-items-img" src="images/Decorative Jali/Mandir side/`+i+`.jpg"></a><br>
+      <h2>MandirSide-`+i+`</h2>
+    </div>
+    `;
+}
 
 document.querySelector(".shop-menu").innerHTML = shop_menu_innerhtml;
---
+++ scriptmandir.js
@@ -0,0 +1,13 @@
+var shop_menu_innerhtml = document.querySelector(".shop-menu").innerHTML;
+
+for(var i=1;i<=27;i++)
+{
+    shop_menu_innerhtml += `
+    <div class="shop-menu-items">
+      <a href="shop-jali.html"><img class="shop-menu-items-img" src="images/mandir/`+i+`.jpg"></a><br>
+      <h2>Mandir-`+i+`</h2>
+    </div>
+    `;
+}
+
+document.querySelector(".shop-menu").innerHTML = shop_menu_innerhtml;
