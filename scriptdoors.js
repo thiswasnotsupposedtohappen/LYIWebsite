8adac34b9942cc20ad2d550a9845ec0f8aacaf42
thiswasnotsupposedtohappen <abhimanyumidnight@hotmail.com>
2024-11-17 00:03:27 +0530

    0.0.3

--- scriptdoors.js
+++ scriptdoors.js
@@ -5,7 +5,7 @@ for(var i=1;i<=46;i++)
     shop_menu_innerhtml += `
     <div class="shop-menu-items">
       <a href="shop-doors.html"><img class="shop-menu-items-img" src="images/Doors/`+i+`.jpg"></a><br>
-      <h2>Door LYI-`+i+`</h2>
+      <h2>Door-`+i+`</h2>
     </div>
     `;
 }
