cd98b1c8da8fd67fadd91af460f8ca535a7a4531
thiswasnotsupposedtohappen <abhimanyumidnight@hotmail.com>
2024-11-22 00:37:13 +0530

    0.0.6

--- scriptjali.js
+++ scriptjali.js
@@ -10,57 +10,196 @@ class JaliSpecifications
     this.materialback = undefined;
     this.thicknessfront = undefined;
     this.thicknessback = undefined;
+    this.colorfront = undefined;
+    this.colorback = undefined;
   }
 }
 var jalispecifications = new JaliSpecifications();
 
+OnClickConstruction(document.querySelector(".shop-jaliconfig-options"), 'Standalone');
 
 function OnClickConstruction(div, type)
 {
+  jalispecifications.construction = type;
+  var index = undefined;
   if(type=='Standalone')
   {
-    jalispecifications.construction = 'Standalone';
     document.querySelector(".shop-jaliconfig-materialfront").style.display="grid"
     document.querySelector(".shop-jaliconfig-materialback").style.display="none"
     document.querySelector(".shop-jaliconfig-materialhalfdepth").style.display="none"
-    div.querySelectorAll(".shop-jaliconfig-button").forEach(element => {element.style.backgroundColor="rgb(146, 228, 255)";});
-    div.querySelectorAll(".shop-jaliconfig-button")[0].style.backgroundColor="rgb(255, 153, 0)";
+    index = 0;
   }
   if(type=='Back-Supported')
   {
-    jalispecifications.construction = 'Back-Supported';
     document.querySelector(".shop-jaliconfig-materialfront").style.display="grid"
     document.querySelector(".shop-jaliconfig-materialback").style.display="grid"
     document.querySelector(".shop-jaliconfig-materialhalfdepth").style.display="none"
-    div.querySelectorAll(".shop-jaliconfig-button").forEach(element => {element.style.backgroundColor="rgb(146, 228, 255)";});
-    div.querySelectorAll(".shop-jaliconfig-button")[1].style.backgroundColor="rgb(255, 153, 0)";
+    index = 1;
 }
   if(type=='Half-Depth')
   {
-    jalispecifications.construction = 'Half-Depth';
     document.querySelector(".shop-jaliconfig-materialfront").style.display="none"
     document.querySelector(".shop-jaliconfig-materialback").style.display="none"
     document.querySelector(".shop-jaliconfig-materialhalfdepth").style.display="grid"
-    div.querySelectorAll(".shop-jaliconfig-button").forEach(element => {element.style.backgroundColor="rgb(146, 228, 255)";});
-    div.querySelectorAll(".shop-jaliconfig-button")[2].style.backgroundColor="rgb(255, 153, 0)";
+    index = 2;
   }
+
+  SelectButton(div.querySelectorAll(".shop-jaliconfig-button"), index);
 }
 
 function OnClickSupplier(div, type)
 {
-  if(type=='LightYear')
-  {
-    jalispecifications.supplier = 'LightYear';
-    div.querySelectorAll(".shop-jaliconfig-button").forEach(element => {element.style.backgroundColor="rgb(146, 228, 255)";});
-    div.querySelectorAll(".shop-jaliconfig-button")[0].style.backgroundColor="rgb(255, 153, 0)";
-  }
-  if(type=='Customer')
-  {
-    jalispecifications.supplier = 'Customer';
-    div.querySelectorAll(".shop-jaliconfig-button").forEach(element => {element.style.backgroundColor="rgb(146, 228, 255)";});
-    div.querySelectorAll(".shop-jaliconfig-button")[1].style.backgroundColor="rgb(255, 153, 0)";
-  }
+  jalispecifications.supplier = type;
+  var index = undefined;
+  if(type == 'LightYear')index = 0;
+  if(type == 'Customer')index = 1;
+  SelectButton(div.querySelectorAll(".shop-jaliconfig-button"), index);
 }
+
+function OnClickSize(div, type)
+{
+  jalispecifications.size = type;
+  var index = undefined;
+  if(type == '1ft x 1ft')index = 0;
+  if(type == '2ft x 1ft')index = 1;
+  if(type == '2ft x 2ft')index = 2;
+  if(type == '4ft x 1ft')index = 3;
+  if(type == '4ft x 2ft')index = 4;
+  if(type == '4ft x 3ft')index = 5;
+  if(type == '4ft x 4ft')index = 6;
+  if(type == '5ft x 2ft')index = 7;
+  if(type == '5ft x 4ft')index = 8;
+  if(type == '6ft x 2ft')index = 9;
+  if(type == '6ft x 4ft')index = 10;
+  if(type == '8ft x 4ft')index = 11;
+  SelectButton(div.querySelectorAll(".shop-jaliconfig-button"), index);
+}
+
+function OnClickProcess(div, type)
+{
+  jalispecifications.process = type;
+  var index = undefined;
+  if(type == 'CNC Laser 0.5mm')index = 0;
+  if(type == 'CNC Router 3mm')index = 1;
+  if(type == 'CNC Router 4mm')index = 2;
+  if(type == 'CNC Router 6mm')index = 3;
+  if(type == 'CNC Router 8mm')index = 4;
+  SelectButton(div.querySelectorAll(".shop-jaliconfig-button"), index);
+}
+
+function OnClickMaterialFront(div, type)
+{
+  jalispecifications.materialfront = type;
+  var index = undefined;
+  if(type == 'Acrylic')index = 0;
+  if(type == 'Corian')index = 1;
+  if(type == 'ACP')index = 2;
+  if(type == 'Laminated Plywood')index = 3;
+  if(type == 'MDF')index = 4;
+  if(type == 'WPC/PVC/Foam')index = 5;
+  if(type == 'Cement Sheet')index = 6;
+  SelectButton(div.querySelectorAll(".shop-jaliconfig-button"), index);
+}
+
+function OnClickMaterialBack(div, type)
+{
+  jalispecifications.materialback = type;
+  var index = undefined;
+  if(type == 'Acrylic')index = 0;
+  if(type == 'Corian')index = 1;
+  if(type == 'ACP')index = 2;
+  if(type == 'Laminated Plywood')index = 3;
+  if(type == 'MDF')index = 4;
+  if(type == 'WPC/PVC/Foam')index = 5;
+  if(type == 'Cement Sheet')index = 6;
+  SelectButton(div.querySelectorAll(".shop-jaliconfig-button"), index);
+}
+
+function OnClickThicknessFront(div, type)
+{
+  jalispecifications.thicknessfront = type;
+  var index = undefined;
+  
+  if(type == '1mm')index = 0;
+  if(type == '2mm')index = 1;
+  if(type == '3mm')index = 2;
+  if(type == '4mm')index = 3;
+  if(type == '5mm')index = 4;
+  if(type == '6mm')index = 5;
+  if(type == '8mm')index = 6;
+  if(type == '10mm')index = 7;
+  if(type == '12mm')index = 8;
+  if(type == '15mm')index = 9;
+  if(type == '18mm')index = 10;
+  if(type == '20mm')index = 11;
+  if(type == '25mm')index = 12;
+  SelectButton(div.querySelectorAll(".shop-jaliconfig-button"), index);
+}
+
+function OnClickThicknessBack(div, type)
+{
+  jalispecifications.thicknessback = type;
+  var index = undefined;
+  
+  if(type == '1mm')index = 0;
+  if(type == '2mm')index = 1;
+  if(type == '3mm')index = 2;
+  if(type == '4mm')index = 3;
+  if(type == '5mm')index = 4;
+  if(type == '6mm')index = 5;
+  if(type == '8mm')index = 6;
+  if(type == '10mm')index = 7;
+  if(type == '12mm')index = 8;
+  if(type == '15mm')index = 9;
+  if(type == '18mm')index = 10;
+  if(type == '20mm')index = 11;
+  if(type == '25mm')index = 12;
+  SelectButton(div.querySelectorAll(".shop-jaliconfig-button"), index);
+}
+
+function OnClickColorFront(div, type)
+{
+  jalispecifications.colorfront = type;
+  var index = undefined;
+  if(type == 'White')index = 0;
+  if(type == 'Black')index = 1;
+  if(type == 'Red')index = 2;
+  if(type == 'Green')index = 3;
+  if(type == 'Blue')index = 4;
+  if(type == 'Orange')index = 5;
+  if(type == 'Brown')index = 6;
+  if(type == 'Yellow')index = 7;
+  if(type == 'Transparent')index = 8;
+  if(type == 'White 60% (040)')index = 9;
+  SelectButton(div.querySelectorAll(".shop-jaliconfig-button"), index);
+}
+
+function OnClickColorBack(div, type)
+{
+  jalispecifications.colorback = type;
+  var index = undefined;
+  if(type == 'White')index = 0;
+  if(type == 'Black')index = 1;
+  if(type == 'Red')index = 2;
+  if(type == 'Green')index = 3;
+  if(type == 'Blue')index = 4;
+  if(type == 'Orange')index = 5;
+  if(type == 'Brown')index = 6;
+  if(type == 'Yellow')index = 7;
+  if(type == 'Transparent')index = 8;
+  if(type == 'White 60% (040)')index = 9;
+  SelectButton(div.querySelectorAll(".shop-jaliconfig-button"), index);
+}
+
+function SelectButton(divquerryselectorall, index)
+{
+  divquerryselectorall.forEach(element => {element.classList.remove("selected");});
+  divquerryselectorall.forEach(element => {element.classList.add("unselected");});   
+  divquerryselectorall[index].classList.remove("unselected");
+  divquerryselectorall[index].classList.add("selected");
+  console.log(jalispecifications)
+}
+
 
 var shop_menu_innerhtml = document.querySelector(".shop-menu").innerHTML;
 for(var i=1;i<=30;i++)
